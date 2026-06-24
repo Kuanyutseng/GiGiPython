b5913fae425779a20477d6f19ab7e0bfe5fef82c
Andy Tseng <kytseng1212@gmail.com>
2026-06-24 21:28:35 +0800

    add code

--- lesson_4/lesson4.py
+++ lesson_4/lesson4.py
@@ -2,8 +2,8 @@
 num1 = 1
 num2 = 2
 num3 = 3
-if num1 > num2 :
-    print("num1比num2大")
+if num3 > num2 :
+    print("num3比num2大")
 elif num1 == num2:
     print("num1跟num2一樣大")
 else:
@@ -32,16 +32,19 @@ if score >= 90:
     if score >= 95:
         print("A+")
     else: #line 34
-        add_point3 = "add_point3"
-        
+        add_point3 = "add_point3" #add_point3 只可以在這段if 裡面使用，在else中是看不到這個add_point3
+        point = 2
         if score >= 93:
             print("A")
-            add_point1 = "add_point1"
+            add_point1 = "add_point1" #add_point1 只可以在這段if 裡面使用，在else中是看不到這個add_point1
+            score += point
         else:
             print("A-")
             add_point2 = "add_point2"
             print(add_point2)
             print(add_point3)
+            score += point
+            
 
         print("inside of 90 and not 95") # 這裡屬於line 34
     print("happy")
@@ -56,13 +59,18 @@ else:
     print("F")
 print("done1")
     
+# else 一定是if裡面最後一個條件
+# elif 一定在if 與else 之間
 
 
+#if 不一定要有elif or else
 
 transcation_threshold = 100
 price = 85
 if transcation_threshold >= 100:
     if price > 90:
         print("sell")
+        #簡訊
     elif price < 80:
         print("buy")
+        # 打電話
