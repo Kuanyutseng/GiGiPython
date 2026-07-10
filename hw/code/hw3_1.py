01e03d8c0d3e0aab6494f26d4ed77e1c5e547cca
Andy Tseng <kytseng1212@gmail.com>
2026-07-10 16:56:46 +0800

    add code

---
+++ hw/code/hw3_1.py
@@ -0,0 +1,12 @@
+# 綜合應用測驗 三 - 題目一：智能超市結帳與庫存更新系統
+# 請在此撰寫你的程式碼
+
+inventory = {
+    "apple": {"price": 30, "stock": 5},
+    "banana": {"price": 15, "stock": 0},  # 已無庫存
+    "orange": {"price": 25, "stock": 3},
+    "peach": {"price": 60, "stock": 2}
+}
+shopping_cart = ["apple", "watermelon", "banana", "orange", "peach"]
+is_vip = True
+total_payment = 0
