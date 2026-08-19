a917cb04d5b6630106d930480aca71d4083bacdb
Andy Tseng <kytseng1212@gmail.com>
2026-08-19 21:07:11 +0800

    add code

---
+++ lesson_8/lesson8.py
@@ -0,0 +1,38 @@
+# 引入json library
+import json
+# object to json
+array = ["apple", "banana", "cherry"]
+print(array)
+
+json_string = json.dumps(array)
+print(json_string)
+
+
+# json to object
+json_array = json.loads(json_string)
+print(json_array)
+
+
+gigi = {
+    "age": 25,
+    "name": "gigi",
+    "student": True,
+    "grades": [99, 88, 77],
+    "address":{
+        "city": "taipei",
+        "distric": "daan"
+    }
+}
+print(gigi)
+
+#將object 存到json
+gigi_json = json.dumps(gigi)
+print(gigi_json)
+
+
+
+
+
+
+
+
