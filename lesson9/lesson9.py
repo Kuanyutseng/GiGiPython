55d6dbef153b24f9f131418777c6824c23f08d14
Andy Tseng <kytseng1212@gmail.com>
2026-08-19 21:30:37 +0800

    add code

---
+++ lesson9/lesson9.py
@@ -0,0 +1,18 @@
+num1 = 1
+num2 = 0
+num3 = "3"
+try:
+    print(num1 / num2)
+    print(num3 / num2)
+except ZeroDivisionError as e:
+    print("this is my ZeroDivisionError: " + str(e))
+except TypeError as e:
+    print("this is my TypeError: " + str(e))
+except Exception as e:
+    print("this is my error: " + str(e))
+finally:
+    print("done try")    
+print("done")
+
+
+
